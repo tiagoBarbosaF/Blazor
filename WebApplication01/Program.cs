8ca7cc8ddafcb1de7b0502f2d1c607cf510f743c
Tiago Barbosa <tiago@farias.cc>
2021-09-11 19:17:20 -0300

    Routes and configurations

--- WebApplication01/Program.cs
+++ WebApplication01/Program.cs
@@ -1,25 +1,52 @@
 using System;
-using System.Net.Http;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace WebApplication01
 {
-    public class Program
+  public class Program
+  {
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
+      var hostname = builder.Configuration["Hostname"];
+      var http = new HttpClient()
+      {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+      };
+      var vehicleData = new Dictionary<string, string>()
+      {
+        {"color", "blue"},
+        {"type", "car"},
+        {"wheels:count", "3"},
+        {"wheels:brand", "Blazin"},
+        {"wheels:brand:type", "rally"},
+        {"wheels:year", "2008"},
+      };
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      var memoryConfig = new MemoryConfigurationSource {InitialData = vehicleData};
 
-            await builder.Build().RunAsync();
-        }
+      builder.Configuration.Add(memoryConfig);
+      builder.Services.AddScoped(sp => http);
+
+      using var response = await http.GetAsync("cars.json");
+      using var stream = await response.Content.ReadAsStreamAsync();
+
+      builder.Configuration.AddJsonStream(stream);
+
+      builder.RootComponents.Add<App>("#app");
+
+      builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
+      builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+
+      await builder.Build().RunAsync();
     }
+  }
 }
